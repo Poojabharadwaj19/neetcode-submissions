bb1db50c878fd1085de4df51e9bb950ee0773937
Poojabharadwaj19 <91372544+Poojabharadwaj19@users.noreply.github.com>
2026-08-07 23:49:34 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,27 @@
+
+
+public class Solution {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count characters in s
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // Decrement counts using t
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+
+        return true;
+    }
+}
+
